6dcd1a5a067ff0d499be17d68f0661726a949d91
Palden <kazamanemesis1811@gmail.com>
2023-08-02 00:18:26 +0545

    Controller,Services and repo added

--- src/main/java/com/sheryians/major/service/ProductService.java
+++ src/main/java/com/sheryians/major/service/ProductService.java
@@ -7,12 +7,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ProductService {
     @Autowired
     ProductRepo productRepo;
     public List<Product> getAllProduct(){
         return productRepo.findAll();
+    }
+    public void addProduct(Product product){
+        productRepo.save(product);
+    }
+    public void removeProductById(long id){
+        productRepo.deleteById(id);
+    }
+    public Optional<Product>getProductById(long id){
+        return productRepo.findById(id);
+    }
+    public List<Product>getAllProductsByCategoryId(int id){
+        return productRepo.findAllByCategory_Id(id);
     }
 
 }
