69412c8322886e1661384935d4874c5f45bb82c8
Palden <kazamanemesis1811@gmail.com>
2023-08-02 01:14:31 +0545

    Auth added

--- src/main/java/com/sheryians/major/configuration/SecurityConfig.java
+++ src/main/java/com/sheryians/major/configuration/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.sheryians.major.configuration;
 
+import com.sheryians.major.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,6 +17,10 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    GoogleOAuth2SuccessHandler googleOAuth2SuccessHandler;
+    @Autowired
+    CustomUserDetailService customUserDetailService;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
---
+++ src/main/java/com/sheryians/major/repository/RoleRepo.java
@@ -0,0 +1,7 @@
+package com.sheryians.major.repository;
+
+import com.sheryians.major.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepo extends JpaRepository<Role,Integer> {
+}
